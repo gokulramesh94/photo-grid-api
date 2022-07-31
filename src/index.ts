c35d7a176ef71f8bb04501285a4b0d60da3390d1
Gokul <gokulpulari@gmail.com>
2022-07-31 07:52:02 -0400

    Establish mongoose connection and set port.

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+dotenv.config();
+
+const MONGO_URI = process?.env?.MONGO_URI || '';
+const PORT = process?.env?.PORT || 80;
+
+const app = express();
+
+//Enable cors
+app.use(cors());
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(express.json({ limit: '30mb' }));
+
+mongoose
+  .connect(MONGO_URI)
+  .then((response) => {
+    console.log('mongoose connection response : ', response);
+    console.log('listening on port : ', PORT);
+    app.listen(PORT);
+  })
+  .catch((err) => console.log(err));
+
+export { app };
